30ffe02ce9ac02e68e0abbfb1ae82c9c61debd15
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2023-02-12 06:58:05 +0100

    update/media service

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -1,15 +1,15 @@
-import { v2 } from "cloudinary"
+import { UploadApiResponse, v2 } from "cloudinary"
 
 export default class MediaService {
 
     async uploadImage (
         file: Express.Multer.File,
         folder: string
-    ) {
+    ): Promise<UploadApiResponse> {
         return new Promise((resolve, reject) => {
             return v2.uploader.upload(file.path, { folder }, (error, value) => {
                 if (error) reject(error)
-                resolve(value)
+                resolve(value as UploadApiResponse)
             })
         })
     }
