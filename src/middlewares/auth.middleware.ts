63f9597ba8e972416eb63cd9c95093f4e74ba887
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2023-02-12 06:56:47 +0100

    fix/catch exception error

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -5,41 +5,46 @@ import database from "../database";
 import AuthService from "../services/auth.service";
 import HttpException from "../utils/exception";
 
-const authMiddleware:RequestHandler = async ( 
-    req: Request, 
-    res: Response ,
-    next: NextFunction 
+const authMiddleware: RequestHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
 
-    const token = req.headers.authorization?.split(' ')[1]
-    const authService = new AuthService()
-    const ormServices = database.getClient()
+    try {
+        const token = req.headers.authorization?.split(' ')[1]
+        const authService = new AuthService()
+        const ormServices = database.getClient()
 
-    if (!token) {
-        throw new HttpException(
-            StatusCodes.UNAUTHORIZED,
-            'Unauthorized'
-        )
-    }
-
-    const userPayload = authService.verifyJwt(token)
-    
-    if (!userPayload) {
-        throw new HttpException(
-            StatusCodes.UNAUTHORIZED,
-            'Invalid Token'
-        )
-    }
-
-    const user = await ormServices.user.findFirst({
-        where: {
-            id: userPayload.id
+        if (!token) {
+            throw new HttpException(
+                StatusCodes.UNAUTHORIZED,
+                'Unauthorized'
+            )
         }
-    })
 
-    req.authUser = user as User
+        const userPayload = authService.verifyJwt(token)
 
-    next()
+        if (!userPayload) {
+            throw new HttpException(
+                StatusCodes.UNAUTHORIZED,
+                'Invalid Token'
+            )
+        }
+
+        const user = await ormServices.user.findFirst({
+            where: {
+                id: userPayload.id
+            }
+        })
+
+        req.authUser = user as User
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+
 
 }
 
