4f91df81466f094248f87d3a7e711535646b5a1c
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2023-02-13 15:01:16 +0100

    update/seed values added

--- src/database/seeds/seed.ts
+++ src/database/seeds/seed.ts
@@ -1,12 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { Evtol, Medication, PrismaClient } from "@prisma/client";
 import AuthService from "../../services/auth.service";
 import { logger } from "../../utils/logger";
+import { readFile } from "node:fs/promises"
 
 const prisma = new PrismaClient()
 const authService = new AuthService()
 
 async function main() {
 
+    // ADMIN SEED
     try {
 
         logger.info(`🌱 [Seeding]: Admin seed is running`)
@@ -27,9 +29,91 @@ async function main() {
         logger.info(`🌱 [Seeding]: Admin seed complete`)
 
     } catch (error) {
-        logger.error("Error seeding database")
+        logger.error(`🌱 [Seeding]: Admin seed failed`)
         console.log(error)
     }
+    // ADMIN SEED END
+
+    // EVTOL SEED
+    try {
+        
+        logger.info(`🌱 [Seeding]: Evtol is running`)
+
+        const data = await readFile(
+            __dirname + '/evtol.json',
+            'utf-8',
+        )
+
+        const json: Evtol[] = JSON.parse(data)
+
+        json.forEach(async (ev) => {
+            try {
+
+                await prisma.evtol.upsert({
+                    where: {
+                        serial_number: ev.serial_number
+                    },
+                    create: {
+                        serial_number: ev.serial_number,
+                        model: ev.model,
+                        weight_limit: ev.weight_limit
+                    },
+                    update: {}
+                })
+
+            } catch (error) {
+                logger.warn(`🌱 [Seeding]: Seeding Error`)
+            }
+        });
+
+        logger.info(`🌱 [Seeding]: Evtol seed complete`)
+
+    } catch (error) {
+        logger.error(`🌱 [Seeding]: Evtol seed failed`)
+        console.log(error)
+    }
+    // EVTOL SEED END
+
+    // MEDICATION SEED
+    try {
+        
+        logger.info(`🌱 [Seeding]: Medication is running`)
+
+        const data = await readFile(
+            __dirname + '/medication.json',
+            'utf-8',
+        )
+
+        const json: Medication[] = JSON.parse(data)
+
+        json.forEach(async (md) => {
+            try {
+
+                await prisma.medication.upsert({
+                    where: {
+                        name: md.name
+                    },
+                    update: {},
+                    create: {
+                        name: md.name,
+                        weight: md.weight,
+                        code: md.code,
+                        image: md.image
+                    }
+                })
+
+            } catch (error) {
+                logger.warn(`🌱 [Seeding]: Seeding Error`)
+            }
+        });
+
+        logger.info(`🌱 [Seeding]: Medication seed complete`)
+
+    } catch (error) {
+        logger.error(`🌱 [Seeding]: Medication seed failed`)
+        console.log(error)
+    }
+    // MEDICATION SEED END
 
 }
 
